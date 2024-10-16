0f4589ae697be5f23288cea4c149759030aa145f
SandroBarillaPXL <sandrobarilla2001@gmail.com>
2024-10-16 09:57:57 +0200

    reach page 5

--- puppeteer_torfs.js
+++ puppeteer_torfs.js
@@ -16,15 +16,25 @@ const listShoes = async () => {
     await cookieBtn.click();
   }
 
-  await page.evaluate(() => {
-    const button = document.querySelector(".btn.btn-primary.px-5");
-    if (button) {
-      button.scrollIntoView({ behavior: 'smooth', block: 'center' });
-      button.click();
-    }
-  });
-  console.log("Next page clicked using evaluate()");
-  
+  for (let i = 0; i < 5; i++) {
+    // Wait for the next button to appear
+    await page.waitForSelector(".btn.btn-primary.px-5", { visible: true, timeout: 10000 });
+    console.log(`Next button is visible - Attempt ${i + 1}`);
+
+    // Use evaluate method to scroll into view and click the next button
+    await page.evaluate(() => {
+      const nextButton = document.querySelector(".btn.btn-primary.px-5");
+      if (nextButton) {
+        nextButton.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        nextButton.click();
+      }
+    });
+    console.log(`Next page clicked using evaluate method - Attempt ${i + 1}`);
+
+    // Wait for navigation or changes after clicking the next button
+    await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+    console.log(`Navigation to next page completed - Attempt ${i + 1}`);
+  }
 
   //await browser.close();
 };
